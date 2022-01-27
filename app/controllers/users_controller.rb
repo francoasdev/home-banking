baeeea6a2139aff57bc173fa272e99cfe5482446
Franco <francoezapata1@gmail.com>
2022-01-27 13:57:16 -0300

    feat-auth: Added changes in user controller and route for auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,2 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
-  def create
-    user = User.create(user_params)
-    if user.save
-      render json: UserRepresenter.new(user).as_json, status: :created
-    else
-      render json: { error: user.errors.full_messages.first }, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
 end
